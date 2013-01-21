e11de65a853a09708097d15e1e65d707c551b52b
charles.css27 <charles.css27@9ca79ee9-11d0-f85f-0492-87a16fb99f84>
2013-01-21 10:13:30 +0000

    hihihihih

---
+++ pokerProject/src/poker/Poker.java
@@ -0,0 +1,52 @@
+package poker;
+
+/**
+ * The Class Poker.
+ */
+public class Poker {
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isFullHouse(String cards[], int n) {
+		return isThreeOfaKind(cards, n) && isTwoPairs(cards, n);
+	}
+	// Precondition: A hand of n cards have been sorted by card number.
+	/**
+	 * Checks if is three ofa kind.
+	 *
+	 * @param cards the cards
+	 * @param n the n
+	 * @return true, if is three ofa kind
+	 */
+	public boolean isThreeOfaKind(String cards[], int n) {
+		for (int i=0; i<n-2; i++) {
+			if (cards[i].charAt(1) == cards[i+1].charAt(1) &&
+			cards[i+1].charAt(1) == cards[i+2].charAt(1))
+			return true;
+		}
+		return false;
+	}
+	// Precondition: A hand of n cards have been sorted by card number.
+	public boolean isTwoPairs(String cards[], int n) {
+		int count = 0;
+		for (int i=0; i<n-1; i++) {
+		if (cards[i].charAt(1) == cards[i+1].charAt(1)) {
+		count++;
+		i++;
+		}
+		}
+		if (count == 2)
+		return true;
+		else
+		return false;
+	}
+	public static void main(String args[]) {
+	System.out.println(new Poker().isFullHouse(
+	new String[] {"C2", "D2", "H2", "S3", "S4"}, 5));
+	}
+	public void testFindBugs()
+	{
+	String a=null;
+	if(a.equals(""))return;
+	System.exit(1);
+	}
+}
+// end of Poker.java
